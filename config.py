46db4ce4cd80baf8f432ea7a3495aaaccf050898
Sadhika Tadinada <sadhika.tadinada@gmail.com>
2026-09-18 21:26:20 +0530

    Add RBI scraper and modular folder structure

---
+++ config.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent
+
+PROJECT_NAME = "MacroVerba"
+
+RAW_DATA_DIR = PROJECT_ROOT / "data" / "raw"
+PROCESSED_DATA_DIR = PROJECT_ROOT / "data" / "processed"
